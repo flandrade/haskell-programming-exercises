9fa9b1decac610f1f08b07b9840ed3e2226263a5
flandrade <flandradeg@gmail.com>
2016-07-04 18:38:25 -0500

    Add standard functions to chapter exercises

--- Chapter09/Exercises/exercises.hs
+++ Chapter09/Exercises/exercises.hs
@@ -48,3 +48,71 @@ firstLetter' = toUpper . head
 -- Ciphers
 --------------------------------------------
 -- Ciphers.hs
+
+--------------------------------------------
+-- Writing your own standard functions
+--------------------------------------------
+myAnd :: [Bool] -> Bool
+myAnd [] = True
+myAnd (x:xs) = x && myAnd xs
+
+-- 1. myOr returns True if any Bool in the list is True.
+myOr :: [Bool] -> Bool
+myOr []     = False
+myOr (x:xs) = x || myOr xs
+
+-- 2. myAny returns True if a -> Bool applied to any of the values in
+-- the list returns True.
+myAny :: (a -> Bool) -> [a] -> Bool
+myAny _ []     = False
+myAny f (x:xs) = f x || myAny f xs
+
+-- 3 .After you write the recursive myElem, write another version that
+-- uses any.
+myElem :: Eq a => a -> [a] -> Bool
+myElem _ []     = False
+myElem a (x:xs) = (a==x) || myElem a xs
+
+myElem' :: Eq a => a -> [a] -> Bool
+myElem' a = myAny (==a)
+
+-- 4. Implement myReverse.
+myReverse :: [a] -> [a]
+myReverse []   = []
+myReverse (x:xs) = myReverse xs ++ [x]
+
+-- 5. squish flattens a list of lists into a list
+squish :: [[a]] -> [a]
+squish []     = []
+squish [x]    = x
+squish (x:xs) = x ++ squish xs
+
+-- 6. squishMap maps a function over a list and concatenates the results.
+squishMap :: (a -> [b]) -> [a] -> [b]
+squishMap _ []     = []
+squishMap f (x:xs) = f x ++ squishMap f xs
+
+-- 7. squishAgain flattens a list of lists into a list. This time re-use
+-- the squishMap function.
+squishAgain :: [[a]] -> [a]
+squishAgain = squishMap id
+
+-- 8. myMaximumBy takes a comparison function and a list and returns
+-- the greatest element of the list based on the last value that the
+-- comparison returned GT for.
+myMaximumBy :: (a -> a -> Ordering) -> [a] -> a
+myMaximumBy = undefined
+
+-- 9. myMinimumBy takes a comparison function and a list and returns
+-- the least element of the list based on the last value that the com-
+-- parison returned LT for.
+myMinimumBy :: (a -> a -> Ordering) -> [a] -> a
+myMinimumBy = undefined
+
+-- 10. Using the myMinimumBy and myMaximumBy functions, write your
+-- own versions of maximum and minimum .
+myMaximum :: (Ord a) => [a] -> a
+myMaximum = undefined
+
+myMinimum :: (Ord a) => [a] -> a
+myMinimum = undefined
