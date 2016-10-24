6a7f66d2789800bd22df0198cf7a4d2a84b60378
Fernanda Andrade <flandradeg@gmail.com>
2016-10-23 23:59:23 -0500

    Add intermission exercises: 11.8

--- Chapter11/Intermission/exercises.hs
+++ Chapter11/Intermission/exercises.hs
@@ -5,7 +5,7 @@
 module Chapter11.Intermission where
 
 --------------------------------------------
--- 11.4. Exercises: Dog Types
+-- 11.5. Exercises: Dog Types
 --------------------------------------------
 
 data PugType = PugData
@@ -46,8 +46,9 @@ data Doggies a =
 -- 9. What is the type of DogueDeBordeaux "doggie!"
 --    DogueDeBordeaux [Char]
 
+
 --------------------------------------------
--- 11.5. Exercises: Vehicles
+-- 11.6. Exercises: Vehicles
 --------------------------------------------
 
 data Price = Price Integer deriving (Eq, Show)
@@ -106,3 +107,34 @@ doge' = Plane' PapuAir (Size 300)
 isPlane' :: Vehicle' -> Bool
 isPlane' (Plane' _ _) = True
 isPlane' _            = False
+
+
+--------------------------------------------
+-- 11.8. Exercises: Cardinality
+--------------------------------------------
+
+-- While we haven’t explicitly described the rules for calculating the
+-- cardinality of datatypes yet, you might already have an idea of how to do
+-- it for simple datatypes with nullary constructors.
+
+-- 1.
+data PugType = PugData
+-- It's 1
+
+-- 2. For this one, recall that Bool is also defined with the |:
+data Airline =
+       PapuAir
+     | CatapultsR'Us
+     | TakeYourChancesUnited
+-- It's 2
+
+-- 3. Given what we know about Int8, what’s the cardinality of Int16?
+-- It's 65536
+
+-- 4. Use the REPL and maxBound and minBound to examine Int and Integer.
+-- What can you say about the cardinality of those types?
+-- minBound and maxBound is not defined for Integer.
+
+-- 5. Extra credit (impress your friends!): What’s the connection between the
+-- 8 in Int8 and that type’s cardinality of 256?
+-- Int8 = 2 ^ 8 = 256 
